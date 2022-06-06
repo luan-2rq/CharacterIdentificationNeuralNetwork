b7e7d0fd0e8aa6b71cabda204774a85326824585
Otavio Rosa <otavio.nunes021@gmail.com>
2022-06-05 22:30:17 -0300

    starting model

---
+++ model/MLP.py
@@ -0,0 +1,17 @@
+class MLP:
+    def __init__(self, input_neurons, output_neurons, hidden_layers):
+        self.input_neurons = input_neurons
+        self.output_neurons = output_neurons
+        self.hidden_layers = hidden_layers
+
+    def init_weights():
+        #iniciar pesos aleatoriamente
+        pass
+
+    def back_propagate():
+        #calcular novos pesos para caso uma predição tenha sido errada
+        pass
+
+    def train(data, label):
+        #este metodo sera responsavel pelo treinamento da rede e deve receber um conjunto de dados juntamente com os rotulos de cada vetor de dado
+        pass
